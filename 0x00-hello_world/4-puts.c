aad9d54c9675215cedeb43e993cfc99706364da9
techgirlfiona <josephfavouray42@gmail.com>
2023-02-16 16:18:14 +0000

    Betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- *  * main - Entry point,
- *   * Print a line of code using puts
- *    * ends with a new line
- *     *
- *      * Return: Always 0 (Success)
- *       */
+ *  main - Entry point,
+ *  Print a line of code using puts
+ *  ends with a new line
+ * 
+ *  Return: Always 0 (Success)
+ */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
