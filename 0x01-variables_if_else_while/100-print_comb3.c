934269a0800855be9a692f6377861e9988bb2a6e
techgirlfiona <josephfavoura --global user.email josephfavouray42@gmail.comgit config --global user.name techfirlfiona>
2023-02-18 10:19:13 -0800

    So she doesnt stress me

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Write a program that prints all possible different combinations
+ * of two digits.
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	int digit0, digit1;
+
+	for (digit0 = 0; digit0 < 9; digit0++)
+	{
+		for (digit1 = digit0 + 1; digit1 < 10 ; digit1++)
+		{
+			putchar((digit0 % 10) + '0');
+
+			putchar((digit1 % 10) + '0');
+
+	if (digit0 == 8 && digit1 == 9)
+		continue;
+	putchar(',');
+	putchar(' ');
+
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
