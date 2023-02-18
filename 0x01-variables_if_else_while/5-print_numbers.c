c3ebd201894e04eeadb11a958563bd8c01aa0789
techgirlfiona <josephfavoura --global user.email josephfavouray42@gmail.comgit config --global user.name techfirlfiona>
2023-02-18 07:22:55 -0800

    Betty is my friend

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - program that prints all single digit numbers of base 10 from 0
+ * Return: retun 0.
+ */
+int main(void)
+{
+	int num;
+	
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+	printf("\n");
+	return (0);
+}
