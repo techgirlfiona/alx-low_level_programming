a90a2e76f9a3ab1ee7f612dc54bda6bf2da533ee
techgirlfiona <josephfavoura --global user.email josephfavouray42@gmail.comgit config --global user.name techfirlfiona>
2023-02-18 03:42:40 -0800

    Betty my dear

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,12 @@
 #include <stdlib.h>
-#include <time.h>
+#include  <time.h>
 #include <stdio.h>
 /**
- * main - n will store a different value every time you run this program
+ * main - This program will assign a random number to the variable n
+ * each time it is executed.print whether the number stored in the variable
+ * is positive or negative
  *
- * Return: return 0 this time.
+ * Return: Always 0.
  */
 int main(void)
 {
@@ -13,18 +15,16 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if ((n % 10) > 5)
+	if (n > 0)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
+		printf("%d is positive\n", n);
 	}
-	else if ((n % 10) < 6 && (n % 10) != 0)
+	else if (n < 0)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
+		printf("%d is negative\n", n);
 	}
 	else
-	{
-		printf("Last digit of %d is %d and is 0\n", n, n % 10);
-	}
+		printf("%d is zero\n", n);
 
 	return (0);
 }
