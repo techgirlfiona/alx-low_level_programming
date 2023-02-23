5e4a6ac0232c70321d406fe9316ffe5a043d572a
techgirlfiona <josephfavoura --global user.email josephfavouray42@gmail.comgit config --global user.name techfirlfiona>
2023-02-23 14:47:10 -0800

    Mule

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - multiples two integer
+ * @a: the first integer to be multiplied
+ * @b: the second integer to be multiplied
+ * Return: the result of multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
