3ad0537887696412186a6b195553fb3004e235e9
techgirlfiona <josephfavoura --global user.email josephfavouray42@gmail.comgit config --global user.name techfirlfiona>
2023-02-23 04:46:37 -0800

    Largest number

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include  <time.h>
+#include <stdio.h>
+/**
+ * main - This program will assign a random number to the variable n
+ * each time it is executed.print whether the number stored in the variable
+ * is positive or negative
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else
+		printf("%d is zero\n", n);
+
+	return (0);
+}
