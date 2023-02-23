75047f760cab1378f46f60fe8c5bed03014fe774
techgirlfiona <josephfavoura --global user.email josephfavouray42@gmail.comgit config --global user.name techfirlfiona>
2023-02-23 04:58:39 -0800

    Stressing me

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,30 +1,29 @@
-#include <stdlib.h>
-#include  <time.h>
-#include <stdio.h>
+#include "main.h"
+
 /**
- * main - This program will assign a random number to the variable n
- * each time it is executed.print whether the number stored in the variable
- * is positive or negative
- *
- * Return: Always 0.
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
  */
-int main(void)
+
+int largest_number(int a, int b, int c)
 {
-	int n;
+	int largest;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-
-	if (n > 0)
+	if (a >= b && a >= c)
 	{
-		printf("%d is positive\n", n);
+		largest = a;
 	}
-	else if (n < 0)
+	else if (b >= a && b >= c)
 	{
-		printf("%d is negative\n", n);
+		largest = b;
 	}
-	else
-		printf("%d is zero\n", n);
+	else if (c >= a && c >= b)
+	{
+		largest = c;
+	}
 
-	return (0);
+	return (largest);
 }
