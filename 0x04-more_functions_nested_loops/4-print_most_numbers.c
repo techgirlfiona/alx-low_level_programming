e7f121fb6fc2af89c5938e0f962defca6dd75987
techgirlfiona <josephfavoura --global user.email josephfavouray42@gmail.comgit config --global user.name techfirlfiona>
2023-02-23 19:41:54 -0800

    Doing hard things

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_most_numbers - print number except 2 and 4
+ *
+ * return: void
+ */
+void print_most_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		if (!(c == '2' || c == '4'))
+			putchar(c);
+	}
+	putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,4 +5,5 @@ int _isupper(int c);
 int mul(int a, int b);
 int _isdigit(int c);
 void print_numbers(void);
+void print_most_numbers(void);
 #endif
