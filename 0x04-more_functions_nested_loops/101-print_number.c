9ec98216cb17a3698741b084658e5c678700f523
techgirlfiona <josephfavoura --global user.email josephfavouray42@gmail.comgit config --global user.name techfirlfiona>
2023-02-24 03:01:26 -0800

    Betty

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,19 +1,25 @@
 #include "main.h"
 /**
- * print_number - prints integer
+ * print_number - prints an integer
  * @n: integer to be printed
  */
 void print_number(int n)
 {
-	unsigned int i = n;
+	unsigned int n1;
 
 	if (n < 0)
 	{
-		putchar('-');
-		i = -i;
-	}
-	if ((i / 10) > 0)
-		print_number(i / 10);
-	putchar((i % 10) + '0');
+		n1 = -n;
 
+		_putchar('-');
+	}
+	else
+	{
+		n1 = n;
+	}
+	if (n1 / 10)
+	{
+		print_number(n1 / 10);
+	}
+	_putchar((n1 % 10) + '0');
 }
