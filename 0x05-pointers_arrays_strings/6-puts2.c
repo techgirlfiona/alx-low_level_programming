d1d4ce40d4b9d67d7fe937b8a6f32d467a973fa5
techgirlfiona <josephfavoura --global user.email josephfavouray42@gmail.comgit config --global user.name techfirlfiona>
2023-02-28 06:30:09 -0800

    Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts2 - a function that prints every other character
+ * of a string strarting with the first character
+ * @str: an input string
+ * Return: Nothing
+ */
+
+void puts2(char *str)
+{
+	int len;
+	int i;
+
+	len = 0;
+	i = 0;
+
+	while (str[len] != '\0')
+		len++;
+	len -= 1;
+
+	for (; i <= len; i += 2)
+		_putchar(str[i]);
+	_putchar('\n');
+}
