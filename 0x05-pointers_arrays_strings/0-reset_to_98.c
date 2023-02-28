806ff824553d3eda97d251c0d0df4537eb433133
techgirlfiona <josephfavoura --global user.email josephfavouray42@gmail.comgit config --global user.name techfirlfiona>
2023-02-28 05:24:15 -0800

    keep pushing

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * reset_to_98.c - a function that takes a pointer to an int as parameter
- * and updates the value it points  
- * 
+ * reset_to_98 - a function that takes a pointer to an int as parameter
+ * and updates the value it points to to 98
+ * @n: input integer
  * Returns nothing (0)
  */
 
 void reset_to_98(int *n)
 {
-	*n=98;
+	*n = 98;
 }
