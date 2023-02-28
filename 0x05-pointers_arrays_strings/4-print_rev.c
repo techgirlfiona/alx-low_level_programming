de87dbe0d714733e50b708aa1e3042e142891985
techgirlfiona <josephfavoura --global user.email josephfavouray42@gmail.comgit config --global user.name techfirlfiona>
2023-02-28 06:18:59 -0800

    Doing

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - a function that prints a string, in reverse,
+ * followed by a new line.
+ * @s: input string
+ * Return: nothing
+ */
+
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	while (len)
+		_putchar(s[--len]);
+	_putchar('\n');
+}
