3b3116a5c1bd8d63755997447f917160d1764a50
techgirlfiona <josephfavoura --global user.email josephfavouray42@gmail.comgit config --global user.name techfirlfiona>
2023-04-15 05:05:38 -0700

    Flip

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * flip_bits - Counts the number of bits needed to be
+ *             flipped to get from one number to another.
+ * @n: The number.
+ * @m: The number to flip n to.
+ *
+ * Return: The necessary number of bits to flip to get from n to m.
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int diff;
+	int counter;
+
+	diff = n ^ m;
+	counter = 0;
+
+	while (diff)
+	{
+		counter++;
+		diff &= (diff - 1);
+	}
+
+	return (counter);
+}
