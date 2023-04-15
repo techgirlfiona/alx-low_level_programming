290c1431813fe1e121089a01da3a48dc9c9be138
techgirlfiona <josephfavoura --global user.email josephfavouray42@gmail.comgit config --global user.name techfirlfiona>
2023-04-15 05:18:46 -0700

    Endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * get_endianness - Checks the endianness.
+ *
+ * Return: If big-endian - 0.
+ *         If little-endian - 1.
+ */
+int get_endianness(void)
+{
+	int num;
+
+	num = 1;
+	if (*(char *)&num == 1)
+		return (1);
+	else
+		return (0);
+}
